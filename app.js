5e4b2ac9cac1b661a91ea15d50e20515423ea08a
Will McCracken <wem5637@gmail.com>
2022-01-16 23:41:59 -0600

    change rtmp to port

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const NodeMediaServer = require('node-media-server');
 
 const config = {
   rtmp: {
-    port: 1935,
+    port: process.env.PORT || 1935,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60
   },
   http: {
-    port: process.env.PORT || 8000,
+    port: 8000,
     mediaroot: './media',
     allow_origin: '*'
   },
