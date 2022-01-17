a56cd21b92019f40abcb3339290445be20814bf5
Will McCracken <wem5637@gmail.com>
2022-01-17 00:01:42 -0600

    whatever

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const NodeMediaServer = require('node-media-server');
 
 const config = {
   rtmp: {
-    port: process.env.PORT || 1935,
+    port: 80,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60
   },
   http: {
-    port: 8000,
+    port: process.env.PORT || 8000,
     mediaroot: './media',
     allow_origin: '*'
   },
