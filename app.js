19a765ce7ed87d95226693f1790527ebb3797213
Will McCracken <wem5637@gmail.com>
2022-02-17 00:19:13 -0600

    post publish and cors

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
-
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"]
+  }
+});
 
 let streamChannel = null;
 app.get('/', (req, res) => {
@@ -60,3 +64,11 @@ const config = {
 
 var nms = new NodeMediaServer(config)
 nms.run();
+
+nms.on('postPublish', (id, StreamPath, args) => {
+  io.broadcast('refetch')
+});
+
+nms.on('postConnect', (id, args) => {
+  io.broadcast('refetch')
+});
